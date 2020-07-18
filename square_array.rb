0ceba5b3e719703990c002c316b05211fad47b8d
Mary Dick <maryclairedick@gmail.com>
2020-07-18 20:08:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,8 @@
 def square_array(array)
   numbers = [1,2,3]
-  count = 0
   numbers.each do |number|
     number = number**2
-    numbers[count] = number
-    count += 1
+    square_array = number
+    
   end
 end
